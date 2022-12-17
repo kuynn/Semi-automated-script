b8bf915a8e25b2cd45f7fcca621635763e36b34f
kuynn <ezegaston2011@gmail.com>
2022-12-17 00:04:16 -0300

    Update semi-automated python script.py

--- semi-automated python script.py
+++ semi-automated python script.py
@@ -2,10 +2,10 @@
 import time
 import pyautogui as pg
 
-print("program will run in 5 secs")
-time.sleep(5)
+print("program will run in 10 secs")
+time.sleep(10)
 
 for i in range(100):
-    pg.write("This is a semi-automated python code!")
+    pg.write("This is a semi-automated python code.")
     time.sleep(5)
     pg.press("Enter")
